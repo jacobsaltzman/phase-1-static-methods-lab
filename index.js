1e31d07e482172c27e4599fa0ebb6ebaf17ab279
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-07-04 20:53:49 -0700

    commit tree 4db0da76a39ad342ab10e05aa4449e6aaba7679b

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    return str[0].toUpperCase() + str.slice(1)
+  }
+  static sanitize(str){
+    return str.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+  static titleize(str){
+    let exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let final = []
+    let newArray = str.split(" ")
+    for (let i = 0; i < newArray.length; i++){
+      if (i == 0){
+        final.push(this.capitalize(newArray[i]))
+      } else{
+        if (exceptions.includes(newArray[i])){
+          final.push(newArray[i])
+        } else {
+          final.push(this.capitalize(newArray[i]))
+        }
+      }
+    }
+    return final.join(" ")
+  }
 }
